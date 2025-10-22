cc8f4d9a66264b7aabbafa73516fecbab8874f9d
Darian Elwood <44715813+DarianElwood@users.noreply.github.com>
2025-10-22 03:19:35 -0500

    Simplify Vite config by removing static copy plugin

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/robots.txt',
-          dest: ''
-        },
-        {
-          src: 'public/sitemap.xml',
-          dest: ''
-        }
-      ]
-    })
-  ]
-});
+  plugins: [react()],
+  base: '/',
+})
